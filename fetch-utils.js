f873e86c62bafbc944baab77365fe999097a8f87
Jon Borgonia <jon@gomagames.com>
2016-08-26 18:19:41 -1000

    positively failing test for GET /api/contributions

---
+++ fetch-utils.js
@@ -0,0 +1,20 @@
+/*
+ * General utils for handling node-fetch results
+ */
+
+// custom encoding function, because querystring.encode will encode the $ => %24
+const qsEncode = params => Object.keys(params).map( key => `${key}=${encodeURIComponent(params[key])}`).join('&');
+
+// return the json formatted body, or an error message
+const fetchJson = response => {
+  if( !response.ok ){
+    const { status, statusText, url } = response;
+    throw { status, statusText, url, body : response.text() };
+  }
+  return response.json();
+};
+
+module.exports = {
+  qsEncode,
+  fetchJson
+};
---
+++ test/index.js
@@ -0,0 +1,26 @@
+const test = require('tape');
+const request = require('supertest');
+const app = require('../server');
+const agent = request.agent(app);
+
+test('Contributions API', route => {
+
+  const contributionsEndpoint = '/api/contributions';
+
+  route.test(`GET ${contributionsEndpoint}`, (should)=> {
+
+    agent.get(contributionsEndpoint)
+      .set('Accept', 'application/json')
+      .expect(200)
+      .expect('Content-Type', /json/)
+      .end((err, res) => {
+        if (err) {
+          return should.fail(err);
+        }
+        should.ok(res.body instanceof Array, 'response body should be an array');
+        should.equal(res.body.length, 100, 'response body should include 100 items, as the default limit');
+        should.end();
+      });
+  });
+
+});
